fafc4024fc3ad6a5e962ae04db4d0fbd19683503
Saravanan Shanmugam <sara251186@gmail.com>
2019-09-27 00:39:36 -0400

    updated the deploy script to run test and start the server locally in background

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,8 +1,12 @@
 @echo off
 cd "c:\Users\saravanan\nodeServer"
+echo "********creating the production environment........."
 mkdir Production
 cd production
+echo "********cloning the repository......................."
 git clone "https://github.com/sara251186/nodeJs-weather-website.git"
 cd nodeJs-weather-website
+echo "********Pre Installing the dependent node modules & build & test and execute start to run the application......................."
 npm install && npm test && npm start
+echo "********Application Deployed and running.........................................."
 EOF
