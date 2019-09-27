d785ffa1b7647d06aa18511c979780eb6154dcb6
Saravanan Shanmugam <sara251186@gmail.com>
2019-09-26 22:55:09 -0400

    updated the deploy script to run test and start the server locally

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,11 +1,10 @@
 @echo off
-cd
 cd "c:\Users\saravanan"
 mkdir Production
 cd production
-cd
 git clone "https://github.com/sara251186/nodeJs-weather-website.git"
 cd nodeJs-weather-website
-cd
 npm install
+npm test
+npm start
 EOF
